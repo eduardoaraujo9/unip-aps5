f57c7e60fe8acf2ae99bffa20e8dc069f5985026
Eduardo Araujo <contatoedu@outlook.com>
2017-02-24 01:02:59 +0000

    Initial Push

---
+++ api/api.php
@@ -0,0 +1,16 @@
+<?php
+
+/******************************************************************************
+ *                                                                            *
+ * API de Chat, desenvolvida como APS do 5.o semestre no curso de Ciencias da *
+ *              Computacao na UNIP em Jan/2017.                               *
+ *                                                                            *
+ * Autor: Eduardo Araujo                                                      *
+ *                                                                            *
+ ******************************************************************************/
+
+
+
+
+
+?>
