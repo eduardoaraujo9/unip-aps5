3bf7e89da41b90d8d0015a64933717fdc01d8fb3
root <root@www.nunes.net.br>
2017-02-27 16:55:53 -0500

    Database file

---
+++ api/database.php
@@ -0,0 +1,18 @@
+<?php
+
+class sql {
+  private $server = "127.0.0.1";
+  private $port = "3306";
+  private $database = "chatapi";
+  private $username = "chatapi";
+  private $password = "ipatahc";
+
+}
+
+
+
+
+
+
+
+?>
