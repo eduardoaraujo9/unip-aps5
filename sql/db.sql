8d6a74f8c17d4e8f36d8a3b3e8bc04696843baaf
Eduardo Araujo <contatoedu@outlook.com>
2017-03-06 21:11:53 -0500

    dummy cleanup

--- sql/db.sql
+++ sql/db.sql
@@ -5,24 +5,21 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `cliente` int(10) NOT NULL,
   `hora` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `tipo` int(1) NOT NULL,
+  `tipo` int(1) DEFAULT '0',
   `dados` varchar(1024) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cliente` (`cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 CREATE TABLE IF NOT EXISTS `clientes` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(64) NOT NULL,
   `email` varchar(128) NOT NULL,
   `senha` varchar(32) DEFAULT NULL,
-  `lastupdate` varchar(64) DEFAULT NULL,
+  `lastupdate` varchar(64) DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
-INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`, `lastupdate`) VALUES
-(1, 'Eduardo Araujo', 'contatoedu@outlook.com', 'teste', NULL);
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 CREATE TABLE IF NOT EXISTS `config` (
   `id` int(10) NOT NULL,
